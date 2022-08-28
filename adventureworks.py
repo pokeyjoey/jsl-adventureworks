efdbe8eb86e4b4ca075962eea122a0344bc4fcb4
Jeff Roche <jkeauctions@gmail.com>
2022-08-28 15:21:57 -0400

    Commit of SalesPerson data queries

--- adventureworks.py
+++ adventureworks.py
@@ -97,4 +97,40 @@ sql = """SELECT st.name, SUM(soh.totaldue) AS total_revenue
             LIMIT 10"""
 print(pd.read_sql(sql, conn))
 
+print('')
+print('')
+print('select from the sales.SalesPerson table')
+sql = "select * from sales.SalesPerson limit 2"
+print(pd.read_sql(sql, conn).T)
+
+print('')
+print('')
+print('Read the SalesTerritory table')
+sql = "select * from sales.SalesTerritory limit 2"
+print(pd.read_sql(sql, conn).T)
+
+print('')
+print('')
+print('Find the salesperson id fo the salesperson who had the most sales in the Southwest Territory in the SalesPerson and the SalesTerritory tables')
+sql = """SELECT sp.businessentityid, sp.salesytd
+            FROM sales.SalesPerson sp
+            ORDER BY sp.salesytd DESC
+            LIMIT 1"""
+print(pd.read_sql(sql, conn))
+
+
+print('')
+print('')
+print('Top three items sold by sales person 276 in the SalesOrderHeader, SalesOrderDetail, and Product tables')
+sql = """SELECT p.name, COUNT(*) AS items_sold
+            FROM sales.SalesOrderHeader soh
+            JOIN sales.SalesOrderDetail sod
+            ON soh.salesorderid = sod.salesorderid
+            JOIN production.product p
+            ON sod.productid = p.productid
+            WHERE soh.salespersonid =  276
+            GROUP BY p.name
+            ORDER BY items_sold DESC
+            LIMIT 3"""
+print(pd.read_sql(sql, conn))
 
